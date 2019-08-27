27806437b4ff5f6a57316b5679f9891ddb762464
User <User@LAPTOP-11OUFMHU>
2019-08-27 16:12:51 +0530

    AreaSquareTry

---
+++ src/com/ssi/AreaSquare/UseApi.java
@@ -0,0 +1,12 @@
+
+package com.ssi.AreaSquare;
+import com.ssi.MyFirstApii.FirstApi;
+public class UseApi {
+	public static void main(String[] args) {
+		FirstApi fa=new FirstApi();
+		fa.rectangleArea(8, 10);
+		fa.squareArea(5);
+	}
+
+
+}
